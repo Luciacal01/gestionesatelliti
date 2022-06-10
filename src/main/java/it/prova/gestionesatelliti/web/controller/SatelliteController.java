f638993195ea89ab5f0b0c6bfde3fb63f82f2b73
Luciacal01 <luciacalabria80@gmail.com>
2022-06-10 16:05:55 +0200

    5° commit: creazione e implementazione homeController e creazione di SatelliteController (da implementare)

---
+++ src/main/java/it/prova/gestionesatelliti/web/controller/SatelliteController.java
@@ -0,0 +1,10 @@
+package it.prova.gestionesatelliti.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/satellite")
+public class SatelliteController {
+
+}
